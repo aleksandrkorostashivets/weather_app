7d0bac75719b38bceca285fc7714bf002ff70733
aleksandrkorostashivets <aleksandr.korostashivets23@gmail.com>
2020-10-18 14:39:53 +0300

    no media

---
+++ src/components/weather_info.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Weather = props => (
+    <div className="infoWeath">
+    {props.city &&
+        <div>
+            <p>location: {props.city}, {props.country}</p>
+            <p>temperature: {props.temp}</p>
+            <p>pressure: {props.pressure}</p>
+            <p>sunset: {props.sunset}</p>
+        </div>
+    }
+    <p className="error">{props.error}</p>
+</div>
+)
+export default Weather;
